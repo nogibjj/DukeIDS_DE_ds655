5e1fdec8aff9df04c0dd49f3d76d7c4b3fd0b52f
Divya Sharma <143483773+DivyaSharma0795@users.noreply.github.com>
2023-09-06 15:38:10 -0400

    Update Pandas_Description.py

--- Pandas_Description.py
+++ Pandas_Description.py
@@ -1,5 +1,5 @@
 #  IDS 706 Week 2 assignment - Creating a function to show descriptive statistics for a dataframe
-import pandas as pd
+#import pandas as pd
 
 def PandasDesc(df):
   return df.describe()

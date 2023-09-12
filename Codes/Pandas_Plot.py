14deca1a19aa661f416ce826cb2bdd5ca5e85af9
Divya Sharma <143483773+DivyaSharma0795@users.noreply.github.com>
2023-09-12 02:11:35 -0400

    Update Pandas_Plot.py

--- Codes/Pandas_Plot.py
+++ Codes/Pandas_Plot.py
@@ -5,7 +5,7 @@ import matplotlib.image as mpimg
 
 def PandasPlot(df):
     groups = df.groupby('species')
-    fig, ax = plt.subplots()
+    ax = plt.subplots()
     for name, group in groups:
         ax.plot(group.sepal_length, group.sepal_width, marker='o', linestyle='', ms=12, label=name)
     ax.legend()

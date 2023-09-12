ed33aea7269711a56f3343e7841a672398adc517
Divya Sharma <143483773+DivyaSharma0795@users.noreply.github.com>
2023-09-12 06:40:56 +0000

    Updating testing logic

--- Codes/Test_Week2_Pandas.py
+++ Codes/Test_Week2_Pandas.py
@@ -3,6 +3,13 @@ import pandas as pd
 from Pandas_Description import PandasDesc
 from Pandas_Plot import PandasPlot
 
+def run_Pandas():
+    Data_Link = "./Resources\iris_data.txt"
+    data = pd.read_csv(Data_Link)
+    df = pd.DataFrame(data)
+    print(PandasDesc(df))
+    PandasPlot(df)
+
 def test_Pandas():
     Data_Link = "https://raw.githubusercontent.com/Opensourcefordatascience/Data-sets/master/Iris_Data.csv"
     data = pd.read_csv(Data_Link)
@@ -10,4 +17,5 @@ def test_Pandas():
     print(PandasDesc(df))
     PandasPlot(df)
 
-test_Pandas()
+
+run_Pandas()

0c46ec8288270f9eda7f06dfac4f8b628cf8dca4
Divya Sharma <143483773+DivyaSharma0795@users.noreply.github.com>
2023-09-12 06:28:18 +0000

    Updated codes to add Summary.md to Resources

--- Codes/Pandas_Plot.py
+++ Codes/Pandas_Plot.py
@@ -5,13 +5,14 @@ import matplotlib.image as mpimg
 
 def PandasPlot(df):
     groups = df.groupby('species')
-    ax = plt.subplots()
+    fig,ax = plt.subplots()
     for name, group in groups:
         ax.plot(group.sepal_length, group.sepal_width, marker='o', linestyle='', ms=12, label=name)
     ax.legend()
     plt.savefig('./Resources/plot image.png')
     plt.show()
     print(plt)
+    print(fig)
     image = mpimg.imread('./Resources/plot image.png')
     plt.imshow(image)
     plt.show()
